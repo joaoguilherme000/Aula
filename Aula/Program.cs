0baa4523a39b5765828031a7b5964cd281f95a28
joaoguilherme <joaoguilhermehig@gmail.com>
2024-02-09 21:22:59 -0300

    Add project files.

---
+++ Aula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string nome = "nada";
+            nome = "nada";
+            Console.WriteLine(nome);
+        }
+    }
+}
