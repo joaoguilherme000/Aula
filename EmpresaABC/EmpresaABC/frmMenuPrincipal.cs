f720a8811a13603695267ebefecd6592310a4e74
joaoguilherme <joaoguilhermehig@gmail.com>
2024-03-01 21:42:22 -0300

    Botão na tela do menu principal

--- EmpresaABC/EmpresaABC/frmMenuPrincipal.cs
+++ EmpresaABC/EmpresaABC/frmMenuPrincipal.cs
@@ -16,5 +16,35 @@ namespace EmpresaABC
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
