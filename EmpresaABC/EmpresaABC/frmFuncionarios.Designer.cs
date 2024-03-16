b595c23cf91c99a9bde5768ee4d2005e2897653f
joaoguilherme000 <joaoguilhermehig@gmail.com>
2024-03-15 22:12:19 -0300

    tab components

--- EmpresaABC/EmpresaABC/frmFuncionarios.Designer.cs
+++ EmpresaABC/EmpresaABC/frmFuncionarios.Designer.cs
@@ -47,6 +47,20 @@ namespace EmpresaABC
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.pnlFuncionarios.SuspendLayout();
             this.gpgDados.SuspendLayout();
             this.SuspendLayout();
@@ -57,7 +71,7 @@ namespace EmpresaABC
             this.btnNovo.Location = new System.Drawing.Point(5, 3);
             this.btnNovo.Name = "btnNovo";
             this.btnNovo.Size = new System.Drawing.Size(104, 48);
-            this.btnNovo.TabIndex = 0;
+            this.btnNovo.TabIndex = 12;
             this.btnNovo.Text = "&Novo";
             this.btnNovo.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnNovo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -77,7 +91,7 @@ namespace EmpresaABC
             this.pnlFuncionarios.Location = new System.Drawing.Point(0, 500);
             this.pnlFuncionarios.Name = "pnlFuncionarios";
             this.pnlFuncionarios.Size = new System.Drawing.Size(785, 61);
-            this.pnlFuncionarios.TabIndex = 2;
+            this.pnlFuncionarios.TabIndex = 11;
             // 
             // btnVoltar
             // 
@@ -85,7 +99,7 @@ namespace EmpresaABC
             this.btnVoltar.Location = new System.Drawing.Point(665, 3);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(112, 48);
-            this.btnVoltar.TabIndex = 6;
+            this.btnVoltar.TabIndex = 18;
             this.btnVoltar.Text = "&Voltar";
             this.btnVoltar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnVoltar.UseVisualStyleBackColor = true;
@@ -98,7 +112,7 @@ namespace EmpresaABC
             this.btnLimpar.Location = new System.Drawing.Point(555, 3);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(104, 48);
-            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.TabIndex = 17;
             this.btnLimpar.Text = "&Limpar";
             this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnLimpar.UseVisualStyleBackColor = false;
@@ -109,7 +123,7 @@ namespace EmpresaABC
             this.btnPesquisar.Location = new System.Drawing.Point(445, 3);
             this.btnPesquisar.Name = "btnPesquisar";
             this.btnPesquisar.Size = new System.Drawing.Size(104, 48);
-            this.btnPesquisar.TabIndex = 4;
+            this.btnPesquisar.TabIndex = 16;
             this.btnPesquisar.Text = "&Pesquisar";
             this.btnPesquisar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnPesquisar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -121,7 +135,7 @@ namespace EmpresaABC
             this.btnExcluir.Location = new System.Drawing.Point(335, 3);
             this.btnExcluir.Name = "btnExcluir";
             this.btnExcluir.Size = new System.Drawing.Size(104, 48);
-            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.TabIndex = 15;
             this.btnExcluir.Text = "E&xcluir";
             this.btnExcluir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnExcluir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -133,7 +147,7 @@ namespace EmpresaABC
             this.btnAlterar.Location = new System.Drawing.Point(225, 3);
             this.btnAlterar.Name = "btnAlterar";
             this.btnAlterar.Size = new System.Drawing.Size(104, 48);
-            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.TabIndex = 14;
             this.btnAlterar.Text = "&Alterar";
             this.btnAlterar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnAlterar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -146,7 +160,7 @@ namespace EmpresaABC
             this.btnCadastrar.Location = new System.Drawing.Point(115, 3);
             this.btnCadastrar.Name = "btnCadastrar";
             this.btnCadastrar.Size = new System.Drawing.Size(104, 48);
-            this.btnCadastrar.TabIndex = 1;
+            this.btnCadastrar.TabIndex = 13;
             this.btnCadastrar.Text = "&Cadastrar";
             this.btnCadastrar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnCadastrar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
@@ -154,6 +168,20 @@ namespace EmpresaABC
             // 
             // gpgDados
             // 
+            this.gpgDados.Controls.Add(this.textBox6);
+            this.gpgDados.Controls.Add(this.textBox5);
+            this.gpgDados.Controls.Add(this.maskedTextBox3);
+            this.gpgDados.Controls.Add(this.textBox4);
+            this.gpgDados.Controls.Add(this.label11);
+            this.gpgDados.Controls.Add(this.label10);
+            this.gpgDados.Controls.Add(this.comboBox1);
+            this.gpgDados.Controls.Add(this.label9);
+            this.gpgDados.Controls.Add(this.label8);
+            this.gpgDados.Controls.Add(this.maskedTextBox4);
+            this.gpgDados.Controls.Add(this.label6);
+            this.gpgDados.Controls.Add(this.label7);
+            this.gpgDados.Controls.Add(this.maskedTextBox2);
+            this.gpgDados.Controls.Add(this.label5);
             this.gpgDados.Controls.Add(this.maskedTextBox1);
             this.gpgDados.Controls.Add(this.label4);
             this.gpgDados.Controls.Add(this.textBox3);
@@ -165,7 +193,7 @@ namespace EmpresaABC
             this.gpgDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gpgDados.Location = new System.Drawing.Point(12, 12);
             this.gpgDados.Name = "gpgDados";
-            this.gpgDados.Size = new System.Drawing.Size(756, 354);
+            this.gpgDados.Size = new System.Drawing.Size(756, 298);
             this.gpgDados.TabIndex = 3;
             this.gpgDados.TabStop = false;
             this.gpgDados.Text = "Dados Pessoais";
@@ -173,7 +201,7 @@ namespace EmpresaABC
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 68);
+            this.label1.Location = new System.Drawing.Point(38, 71);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 20);
             this.label1.TabIndex = 0;
@@ -182,22 +210,22 @@ namespace EmpresaABC
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(103, 65);
+            this.textBox1.Location = new System.Drawing.Point(103, 68);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(91, 26);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(103, 137);
+            this.textBox2.Location = new System.Drawing.Point(103, 113);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(272, 26);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 140);
+            this.label2.Location = new System.Drawing.Point(49, 116);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 20);
             this.label2.TabIndex = 2;
@@ -206,15 +234,15 @@ namespace EmpresaABC
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(454, 137);
+            this.textBox3.Location = new System.Drawing.Point(454, 113);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(281, 26);
-            this.textBox3.TabIndex = 5;
+            this.textBox3.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(392, 140);
+            this.label3.Location = new System.Drawing.Point(392, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 20);
             this.label3.TabIndex = 4;
@@ -224,7 +252,7 @@ namespace EmpresaABC
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(57, 217);
+            this.label4.Location = new System.Drawing.Point(57, 166);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 20);
             this.label4.TabIndex = 6;
@@ -232,11 +260,128 @@ namespace EmpresaABC
             // 
             // maskedTextBox1
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(103, 217);
+            this.maskedTextBox1.Location = new System.Drawing.Point(103, 163);
             this.maskedTextBox1.Mask = "999,999,999-00";
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(284, 26);
-            this.maskedTextBox1.TabIndex = 7;
+            this.maskedTextBox1.Size = new System.Drawing.Size(123, 26);
+            this.maskedTextBox1.TabIndex = 3;
+            this.maskedTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Celular";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(323, 163);
+            this.maskedTextBox2.Mask = "99999-9999";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(95, 26);
+            this.maskedTextBox2.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(450, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Número";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Endereço";
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(103, 206);
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(324, 26);
+            this.maskedTextBox4.TabIndex = 5;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(593, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "CEP";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(46, 252);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Bairro";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(565, 248);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(259, 252);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(59, 20);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Cidade";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(499, 252);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 20);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Estado";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(521, 206);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(53, 26);
+            this.textBox4.TabIndex = 6;
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(640, 206);
+            this.maskedTextBox3.Mask = "99999-999";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(85, 26);
+            this.maskedTextBox3.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(103, 249);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(123, 26);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(323, 249);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(123, 26);
+            this.textBox6.TabIndex = 9;
             // 
             // frmFuncionarios
             // 
@@ -277,5 +422,19 @@ namespace EmpresaABC
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
     }
 }
--- EmpresaABC/EmpresaABC/frmFuncionarios.cs
+++ EmpresaABC/EmpresaABC/frmFuncionarios.cs
@@ -53,5 +53,10 @@ namespace EmpresaABC
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
