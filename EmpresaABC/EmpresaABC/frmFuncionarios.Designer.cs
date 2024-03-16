37769ff3c3abbdc2ea4e57e046c92d686423ba24
joaoguilherme000 <joaoguilhermehig@gmail.com>
2024-03-15 21:51:11 -0300

    um group box

--- EmpresaABC/EmpresaABC/frmFuncionarios.Designer.cs
+++ EmpresaABC/EmpresaABC/frmFuncionarios.Designer.cs
@@ -38,7 +38,17 @@ namespace EmpresaABC
             this.btnExcluir = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
             this.btnCadastrar = new System.Windows.Forms.Button();
+            this.gpgDados = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.pnlFuncionarios.SuspendLayout();
+            this.gpgDados.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnNovo
@@ -142,11 +152,98 @@ namespace EmpresaABC
             this.btnCadastrar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.btnCadastrar.UseVisualStyleBackColor = true;
             // 
+            // gpgDados
+            // 
+            this.gpgDados.Controls.Add(this.maskedTextBox1);
+            this.gpgDados.Controls.Add(this.label4);
+            this.gpgDados.Controls.Add(this.textBox3);
+            this.gpgDados.Controls.Add(this.label3);
+            this.gpgDados.Controls.Add(this.textBox2);
+            this.gpgDados.Controls.Add(this.label2);
+            this.gpgDados.Controls.Add(this.textBox1);
+            this.gpgDados.Controls.Add(this.label1);
+            this.gpgDados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpgDados.Location = new System.Drawing.Point(12, 12);
+            this.gpgDados.Name = "gpgDados";
+            this.gpgDados.Size = new System.Drawing.Size(756, 354);
+            this.gpgDados.TabIndex = 3;
+            this.gpgDados.TabStop = false;
+            this.gpgDados.Text = "Dados Pessoais";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(272, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(454, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(281, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(392, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Senha";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "CPF";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(103, 217);
+            this.maskedTextBox1.Mask = "999,999,999-00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(284, 26);
+            this.maskedTextBox1.TabIndex = 7;
+            // 
             // frmFuncionarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(780, 557);
+            this.Controls.Add(this.gpgDados);
             this.Controls.Add(this.pnlFuncionarios);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
@@ -155,6 +252,8 @@ namespace EmpresaABC
             this.Text = "Loja ABC Funcionarios";
             this.Load += new System.EventHandler(this.frmFuncionarios_Load);
             this.pnlFuncionarios.ResumeLayout(false);
+            this.gpgDados.ResumeLayout(false);
+            this.gpgDados.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -169,5 +268,14 @@ namespace EmpresaABC
         private System.Windows.Forms.Button btnExcluir;
         private System.Windows.Forms.Button btnAlterar;
         private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.GroupBox gpgDados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
--- EmpresaABC/EmpresaABC/frmFuncionarios.cs
+++ EmpresaABC/EmpresaABC/frmFuncionarios.cs
@@ -38,5 +38,20 @@ namespace EmpresaABC
             abrir.Show();
             this.Hide();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
